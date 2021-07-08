39315b1ec3755e64edac8fc30b74a6de985ba4fb
priya-u-13 <66128802+priya-u-13@users.noreply.github.com>
2021-07-08 20:18:53 +0530

    Add files via upload

---
+++ sixth.py
@@ -0,0 +1,25 @@
+'Priya Upwanshi......... learning OpenCV and taking real time video with date and time'
+import cv2
+import datetime
+cap = cv2.VideoCapture(0)
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+#cap.set(3,2000)
+#cap.set(4,2000)
+#print(cap.get(3))
+#print(cap.get(4))
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == True:
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        text = "Width : "+ str(cap.get(3)) + ' Height : '+str(cap.get(4)) # taking the width and height of the frame
+        datet = str(datetime.datetime.now()) # taking real time date and time
+        frame = cv2.putText(frame,datet,(10,50),font,1,(0,255,255),2,cv2.LINE_AA) # putting text on the video or image
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'): 
+            break
+    else:
+        break
+cap.release()
+cv2.destroyAllWindows()
